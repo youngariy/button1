98f0c750122a3671b80c585c13f49ad1acfe38a7
Heo young beom <dudqja8617@naver.com>
2024-09-23 11:30:49 +0900

    modify name,number

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Yeon Ryu")
-b2 = Button(window, text="60221010")
+b1 = Button(window, text="허영범")
+b2 = Button(window, text="60212255")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
